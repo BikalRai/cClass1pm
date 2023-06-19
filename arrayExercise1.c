baeb20e69008e1a1d6030498c088d45f1b4af621
Bikal Rai <bikalrai2785@gmailcom>
2023-06-19 09:23:30 +0545

    array sorting descending order

--- arrayExercise1.c
+++ arrayExercise1.c
@@ -1,1 +1,34 @@
 // WAP to sort array elements in descending order
+
+#include <stdio.h>
+
+int main () {
+	int arr[10], i, j, temp;
+	
+	for (i = 0; i < 10; i++) {
+		printf("Enter element %d\n", i + 1);
+		scanf("%d", &arr[i]);
+	}
+	
+	printf("Elements before sorting\n");
+	for(i = 0; i < 10; i++) {
+		printf("%d\t", arr[i]);
+	}
+	
+	for (i = 0; i < 10; i++) {
+		for (j = i+1; j < 10; j++) {
+			if (arr[i] < arr[j]) {
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	
+	printf("\n\nElements after sorting\n");
+	for(i = 0; i < 10; i++) {
+		printf("%d\t", arr[i]);
+	}
+	
+	return 0;
+}
