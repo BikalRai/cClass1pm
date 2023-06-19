d77a0344a2ed72492cafdaa1a92c98aac2160549
Bikal Rai <bikalrai2785@gmailcom>
2023-06-20 00:05:57 +0545

    2d array exercise

--- 2dArray.c
+++ 2dArray.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main () {
-	int i, j;
 	int arr[2][3] = {1, 2, 3, 4, 5, 6};
 	
 	printf("%d\n", arr[1][1]);
 	printf("%d\n", arr[1][0]);
 	printf("%d\n", arr[0][1]);
+	
+	
 	
 	
 	
--- 2dArrayExercise.c
+++ 2dArrayExercise.c
@@ -1,1 +1,32 @@
 // WAP to take input from user for 3 x 4 matrix and output the sum of the matrix elements
+
+#include <stdio.h> 
+
+int main() {
+	
+	int i, j, matrix[3][4], sum = 0;
+	
+	printf("Enter elements of 3 x 4 matrix\n\n\n");
+	
+	for (i = 0; i < 3; i++) {
+		for(j = 0; j < 4; j++) {
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+	
+	printf("\n\nElements of matrix\n");
+	
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j< 4; j++) {
+			printf("%d\t", matrix[i][j]);
+			sum += matrix[i][j];
+		}
+		
+		printf("\n");
+	}
+	
+	printf("\n\nSum = %d", sum);
+	
+	
+	return 0;
+}
